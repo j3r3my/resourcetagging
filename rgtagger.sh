1d4a17a4d12e83af43a92a975473b4e981babf32
j3r3my <jeremy@jeremyfoster.ca>
2019-06-14 10:23:18 -0600

    updated with notes, added login

--- rgtagger.sh
+++ rgtagger.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+###
+# Script runs through an Azure subscription and puts all of the resource groups (rg) in an array
+# It then looks at the tags on each rg and in a nested loop, applies any missing tags on resources
+#
+# Running multiple times will not create duplicate tags
+# This script (with jq and filtering commands) will handle `tags\ with\ spaces`
+# More information on jq: https://stedolan.github.io/jq/tutorial/
+
+# Usage: ./rgtagger.sh
+# Debug: sh -x rgtagger.sh
+###
+
+az login # force this so we're looking at the right tenant!
 
 for rg in $(az group list --query [].name --output tsv)
 do
